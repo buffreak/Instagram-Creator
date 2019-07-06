b0a62838b97d30225e98740c733468c8b629081d
Rivai <40321291+BuffFreak@users.noreply.github.com>
2019-07-06 14:10:32 +0700

    Update function.php

--- Function/function.php
+++ Function/function.php
@@ -9,7 +9,7 @@ function fetchCurlCookies($source) {
 	return $cookies;
 }
 
-function string($length = 15)
+function stringPass($length = 15)
 {
 	$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
 	$charactersLength = strlen($characters);
@@ -39,6 +39,22 @@ function fetch_value($str,$find_start,$find_end) {
 	$length = strlen($find_start);
 	$end    = strpos(substr($str,$start +$length),$find_end);
 	return trim(substr($str,$start +$length,$end));
+}
+function curlClient($url, $param = false){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	if($param){
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+	}
+	$result = curl_exec($ch);
+	list($header, $body) = explode("\r\n\r\n", $result);
+	return [$header, json_decode($body, true)];
 }
 
 ?>
